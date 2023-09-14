7d1449672f7c130826d766dc1f5ab68976c8336c
Maksim <95shum@gmail.com>
2023-09-14 19:22:44 +0300

    add services

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import App from './components/app/App';
+
+import MarvelService from './services/MarvelService';
+
 import './style/style.scss';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const marvelService = new MarvelService()
+marvelService.getAllCharacters().then(res => res.data.results.forEach(element => {
+  console.log(element)
+}))
 
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
