f7b50028ec2c95ff38cd472c68800fcadf0927a7
Maksim <95shum@gmail.com>
2023-09-20 12:37:53 +0300

    update list and rnd char

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,52 +1,79 @@
+import { Component } from 'react';
 import './charList.scss';
-import abyss from '../../resources/img/abyss.jpg';
+import Spinner from '../spinner/Spinner';
+import Error from '../error/Error';
+import MarvelService from '../../services/MarvelService';
 
-const CharList = () => {
-    return (
-        <div className="char__list">
+class CharList extends Component {
+    state = {
+        charList: [],
+        loading: true,
+        error: false
+    }
+    marvelService = new MarvelService();
+
+    componentDidMount() {
+        this.marvelService.getAllCharacters()
+            .then(this.onCharListLoaded)
+            .catch(this.onError)
+    }
+
+    onCharListLoaded = (charList) => {
+        this.setState({
+            charList,
+            loading: false
+        })
+    }
+
+    onError = () => {
+        this.setState({
+            error: true,
+            loading: false
+        })
+    }
+
+    getListChars = (list) => {
+        const contents = list.map(item => {
+            const picture = item.thumbnail === "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg" 
+  ? {objectFit: 'contain'}
+  : null 
+        return (
+            <li className="char__item"
+                key={item.id}>
+                    <img src={item.thumbnail} alt="abyss" style={picture}/>
+                    <div className="char__name">{item.name}</div>
+                </li>
+        )
+    })
+
+        return (
             <ul className="char__grid">
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item char__item_selected">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
+                {contents}
             </ul>
-            <button className="button button__main button__long">
-                <div className="inner">load more</div>
-            </button>
-        </div>
-    )
+        )
+
+    }
+    render() {
+        const {charList, loading, error} = this.state;
+        
+        const items = this.getListChars(charList);
+
+        const errorMessage = error ? <Error/> : null;
+        const spinner = loading ? <Spinner/> : null;
+        const content = !(loading || error) ? items : null;
+
+        return (
+            <div className="char__list">
+                {errorMessage}
+                {spinner}
+                {content}
+                <button className="button button__main button__long">
+                    <div className="inner">load more</div>
+                </button>
+            </div>
+        )
+    }
+    
 }
 
 export default CharList;
--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,101 +1,118 @@
-import {Component} from 'react'
+import { Component } from "react";
+import MarvelService from "../../services/MarvelService";
 
-import MarvelService from '../../services/MarvelService'
-
-import './randomChar.scss';
-import mjolnir from '../../resources/img/mjolnir.png';
-import Spinner from '../spinner/Spinner';
-import Error from '../error/Error';
+import "./randomChar.scss";
+import mjolnir from "../../resources/img/mjolnir.png";
+import Spinner from "../spinner/Spinner";
+import Error from "../error/Error";
 
 class RandomChar extends Component {
-    constructor(props) {
-        super(props)
-        this.updateChar()
-    }
 
-    state = {
-        char : {},
-        loading: true,
-        error: false
-    }
-    marvelService = new MarvelService();
-    
-    errorMessage = () => {
-        this.setState({error: true,
-        loading: false})
-    }
+  state = {
+    char: {},
+    loading: true,
+    error: false,
+  };
 
-    updateChar = () => {
-        const id = Math.floor(Math.random() * (1011340 - 1011200) + 1011200)
-        this.marvelService
-        .getCharacter(id)
-        .then(char => {
-            this.setState({char,loading : false})
-        })
-        .catch(this.errorMessage)
-    }
-    
-    render() {
-        const {char :{name,description,thumbnail,homepage,wiki},loading,error} = this.state
-        const descrip = (description ? description : 'This character have not discription')
-        .slice(0,100) + '...'
-        const errorMessage = error ? <Error/> : null
-        const load = loading ? <Spinner/> : null
-        if(!name) {
-            return <div className="randomchar">
-                        {load}
-                        {errorMessage}
-                        <div className="randomchar__static">
-                        <p className="randomchar__title">
-                            Random character for today!<br/>
-                            Do you want to get to know him better?
-                        </p>
-                        <p className="randomchar__title">
-                            Or choose another one
-                        </p>
-                        <button className="button button__main">
-                            <div className="inner">try it</div>
-                        </button>
-                        <img src={mjolnir} alt="mjolnir" className="randomchar__decoration"/>
-                        </div>
-                    </div>
-        }
+  
+  marvelService = new MarvelService();
 
-        return (
-            <div className="randomchar">
-                <div className="randomchar__block">
-                    <img src={thumbnail} alt="Random character" className="randomchar__img"/>
-                    <div className="randomchar__info">
-                        <p className="randomchar__name">{name}</p>
-                        <p className="randomchar__descr">
-                           {descrip}
-                        </p>
-                        <div className="randomchar__btns">
-                            <a href={homepage} className="button button__main">
-                                <div className="inner">homepage</div>
-                            </a>
-                            <a href={wiki} className="button button__secondary">
-                                <div className="inner">Wiki</div>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div className="randomchar__static">
-                    <p className="randomchar__title">
-                        Random character for today!<br/>
-                        Do you want to get to know him better?
-                    </p>
-                    <p className="randomchar__title">
-                        Or choose another one
-                    </p>
-                    <button className="button button__main">
-                        <div className="inner">try it</div>
-                    </button>
-                    <img src={mjolnir} alt="mjolnir" className="randomchar__decoration"/>
-                </div>
+  componentDidMount(){
+    this.updateChar()
+  }
+  
+  componentWillUnmount(){
+
+  }
+
+  errorMessage = () => {
+    this.setState({ error: true, loading: false});
+  };
+ onLoading = () => {
+    this.setState({loading: true})
+ }
+
+  updateChar = () => {
+    const id = Math.floor(Math.random() * (1011340 - 1011200) + 1011200);
+    this.onLoading()
+    this.marvelService
+      .getCharacter(id)
+      .then((char) => {
+        this.setState({ char, loading: false });
+      })
+      .catch(this.errorMessage);
+  };
+  // tryChar = () => {
+  //   this.onLoading();
+  //   this.updateChar()
+  // }
+  wievChar = (props) => { 
+    return (
+      <div className="randomchar__block">
+          <img
+            src={props.char.thumbnail}
+            style={props.picture}
+            alt="Random character"
+            className="randomchar__img"
+          />
+          <div className="randomchar__info">
+            <p className="randomchar__name">{props.char.name}</p>
+            <p className="randomchar__descr">{props.descrip}</p>
+            <div className="randomchar__btns">
+              <a href={props.char.homepage} className="button button__main">
+                <div className="inner">homepage</div>
+              </a>
+              <a href={props.char.wiki} className="button button__secondary">
+                <div className="inner">Wiki</div>
+              </a>
             </div>
-        )
+          </div>
+        </div>
+    )
+  }
+  render() {
+    
+    const {
+      char,
+      loading,
+      error,
+    } = this.state;
+    const descrip = (char.description ? char.description : "This character have not discription").slice(
+      0,
+      100
+    ) + "...";
+  const picture = char.thumbnail === "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg" 
+  ? {objectFit: 'contain'}
+  : null 
+
+    const errorMessage = error ? <Error /> : null;
+    const load = loading ? <Spinner /> : null;
+    const charWiev = !(loading || error) ? <this.wievChar char={char} descrip={descrip} picture={picture}/> : null
+      return (
+        <div className="randomchar">
+          {load}
+          {errorMessage}
+          {charWiev}
+          <div className="randomchar__static">
+            <p className="randomchar__title">
+              Random character for today!
+              <br />
+              Do you want to get to know him better?
+            </p>
+            <p className="randomchar__title">Or choose another one</p>
+            <button className="button button__main">
+              <div onClick={this.updateChar} className="inner">try it</div>
+            </button>
+            <img
+              src={mjolnir}
+              alt="mjolnir"
+              className="randomchar__decoration"
+            />
+          </div>
+        </div>
+      );
     }
+  
 }
 
 export default RandomChar;
